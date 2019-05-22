6d13d193f0bb3442223e2ec216cd52d319c82967
MATHIEU CAPO <mathieu.capo@iut.local>
2019-05-22 11:23:27 +0200

    Fix 7.3 activity management

--- src/main/resources/db/migration/R__activity_management.sql
+++ src/main/resources/db/migration/R__activity_management.sql
@@ -1,19 +1,36 @@
-CREATE OR REPLACE FUNCTION add_activity(in_title VARCHAR(200),in_description VARCHAR(200), in_owner_id bigint DEFAULT = get_default_owner().id ) RETURNS activity AS $$
--- faire le default 
-DECLARE 
-	ligne activity%rowtype;
+--
+-- add an activity
+--
+CREATE OR REPLACE FUNCTION add_activity(in_title varchar(500),in_description text, in_owner_id bigint default null) RETURNS activity AS $$
+DECLARE
+    default_owner "user"%rowtype;
+    activity_id bigint;
+    activity_res activity%rowtype;
 BEGIN
-	INSERT INTO activity VALUES(nextval('id_generator'),in_title, in_description, now(), now(),in_owner_id);
-	SELECT * INTO ligne FROM activity WHERE title = in_title;
-	RETURN ligne;
-END
-$$ LANGUAGE plpgsql;
+    if in_owner_id is null then
+        default_owner := get_default_owner();
+        in_owner_id := default_owner.id;
+    end if;
+    activity_id := nextval('id_generator');
+    insert into activity (id, title, description,creation_date, modification_date, owner_id)
+    values(activity_id, in_title, in_description,now(), now(), in_owner_id);
+    select * into activity_res
+    from activity
+    where id = activity_id;
+    return activity_res;
+END;
+$$ language plpgsql;
 
-
-CREATE OR REPLACE FUNCTION find_all_activities(activities_cursor refcursor) RETURNS refcursor AS $$
-BEGIN
-	OPEN activities_cursor FOR SELECT activity.title, user.username FROM activity JOIN "user" ON activity.owner_id = user.id;
-	RETURN activities_cursor;
-END
-$$ LANGUAGE plpgsql;
-	
+--
+-- find all activities
+--
+CREATE OR REPLACE FUNCTION find_all_activities(activities_curs refcursor) RETURNS refcursor AS $$
+begin
+    open activities_curs FOR
+        select act.id as id, title, description, creation_date, modification_date, owner_id, username
+        from activity act left join "user" owner
+                                    on act.owner_id = owner.id
+        order by title, username;
+    return activities_curs;
+END;
+$$ language plpgsql;
